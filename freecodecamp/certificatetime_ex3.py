021f7eb209c780021f6bfe5ad6248348e8c48aac
Jariko29 <138761593+Jariko29@users.noreply.github.com>
2023-07-14 13:08:48 +0000

    finalyisadonedeal

--- freecodecamp/certificatetime_ex3.py
+++ freecodecamp/certificatetime_ex3.py
@@ -64,9 +64,10 @@ test.transfer(50, test2)
 test.get_balance()
 test2.get_balance()
 
-
+print(test.get_balance)
 def create_spend_chart(categories):
     lista = []
+    listaname = []
     summ = 0
     count = 0
     for i in range(len(categories)):
@@ -76,12 +77,10 @@ def create_spend_chart(categories):
                 lista.append(categories[i].ledger[j]['amount'])
                 summ += categories[i].ledger[j]['amount']
     summ = abs(summ)
-    print(lista,summ)
     for i in range(len(lista)):
         try:
             if(lista[i].isalpha() == True):
-                count += 1
-                continue
+                listaname.append(lista[i])
         except:
             temp = int((abs(lista[i])*100)/summ)
             if(temp >= 100):
@@ -89,16 +88,52 @@ def create_spend_chart(categories):
             else:
                 lista[i] = int((temp // 10*10)/10 + 1)
     arithmoi = [i for i in range(0,110,10)]
-    print(count)
+    listanum = []
+    temp = 0
+    for i in range(0,len(lista)-1):
+        if(type(lista[i]) == str and type(lista[i+1]) == str):
+            listanum.append(0)
+            temp = 0
+            if(i == len(lista) - 2):
+                listanum.append(0)
+        if(type(lista[i]) == int and type(lista[i+1]) == int):
+            temp = temp + lista[i+1]
+            if(i == len(lista) - 2):
+                listanum.append(temp)
+        if(type(lista[i]) == int and type(lista[i+1]) == str):
+            listanum.append(temp)
+            temp = 0
+            if(i == len(lista) - 2):
+                listanum.append(0)
+        if(type(lista[i]) == str and type(lista[i+1]) == int):
+            temp += lista[i+1]
+            if(i == len(lista) - 2):
+                listanum.append(temp)
+                
     print('Percentage spent by category')
     for i in range(len(arithmoi)):
-        keno = '  '
-        kiklos = ' '
-        for i in range(count):
-            if(lista[count]):
-        print('{:>3}|  '.format(arithmoi[len(arithmoi)-1-i],kiklos))
-    print(lista)
-    
+        grammi = ''
+        for j in range(len(listanum)):
+            try:
+                if(listanum[j].isalpha() == True):
+                    continue
+            except:
+                if(listanum[j] * 10 >= arithmoi[-1-i]):
+                    grammi += 'o  '
+                else:
+                    grammi += '   '
+        print('{:>3}| {}'.format(arithmoi[len(arithmoi)-1-i],grammi))
+    print('    {}'.format('-'*(len(listaname)*3+1)))
+    l = 0
+    while(l < (len(max(listaname,key=len)))):
+        grammi = ''
+        for i in range(len(listaname)):
+            try:
+                grammi += '{}  '.format(listaname[i][l])
+            except:
+                grammi += '   '
+        print('    ',grammi)
+        l += 1
     
             
 
