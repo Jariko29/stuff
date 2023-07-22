183b44c74f2052ea00ffc4da4d384fb672b933d3
Jariko29 <138761593+Jariko29@users.noreply.github.com>
2023-07-22 16:11:06 +0000

    sxedon teliosa tin 5

--- freecodecamp/certificatetime_ex4.py
+++ freecodecamp/certificatetime_ex4.py
@@ -1,57 +1,69 @@
-import random
-import copy
-random.seed(95)
-class Hat:
-    def __init__(self,**kwargs):
-        self.contents = []
-        for k,v in kwargs.items():
-            for i in range(v):
-                self.contents.append(k)
-    def draw(self,posa):
-        if(posa < len(self.contents)):
-            temp = []
-            for i in range(posa):
-                x = len(self.contents) * random.random()
-                temp.append(self.contents[int(x)])
-                self.contents.pop(int(x))
-            return temp
-        else:
-            return False
-
-hat1 = Hat(blue=3,red=2,green=6)
-#hat2 = Hat(red=5, orange=4)
-#hat3 = Hat(red=5, orange=4, black=1, blue=0, pink=2, striped=9)
-
-def experiment(hat,expected_balls,num_balls_drawn,num_experiments):
-    hat = copy.copy(hat.contents)
-    lista = []
-    for k,v in expected_balls.items():
-        for i in range(v):
-            lista.append(k)
-            
-    prob = 0.0
-    if(num_balls_drawn > len(hat)):
-        prob = 1.0
-        return prob
-    for i in range(num_experiments):
-        temp = []
-        listatemp = []
-        for c in range(len(hat)):
-            listatemp.append(hat[c])
-            
-        for j in range(num_balls_drawn):
-            y = random.randint(0,len(listatemp)-1)
-            temp.append(listatemp[y])
-            listatemp.pop(y)
-        arxiko = len(temp)    
-        for l in range(len(lista)):
-            for o in range(len(temp)):
-                if(lista[l] == temp[o]):
-                    temp.pop(o)
-                    break
-        if(arxiko - len(lista) == len(temp)):
-            prob += 1
-    prob = float(prob / num_experiments)
-    return prob
+class Rectangle(object):
+    def __init__(self,w,h) -> None:
+        self.width = w
+        self.height = h
     
-print(experiment(hat1, expected_balls={"blue":2,"green":1}, num_balls_drawn=4, num_experiments=1000))
+    def __str__(self) -> str:
+        protasi = f'Rectangle(width={self.width}, height={self.height})'
+        return protasi
+    
+    def set_width(self,num) -> None:
+        self.width = num
+        
+    def set_height(self,num) -> None:
+        self.height = num
+        
+    def get_area(self) -> int:
+        return (self.height * self.width)
+        
+    def get_perimeter(self) -> int:
+        return (2 * self.width + 2 * self.height)
+    
+    def get_diagonal(self) -> float:
+        return ((self.width ** 2 + self.height ** 2) ** .5)
+    
+    def get_picture(self) -> str:
+        if(self.width > 50 or self.height > 50):
+            return 'Too big for picture.'
+        else:
+            shape = ''
+            for i in range(self.height):
+                shape += '*' * self.width + '\n'
+            return shape
+
+    def get_amount_inside(self,kouti) -> int:
+        tempw = 0
+        temph = 0
+        if(kouti.width > self.width or kouti.height > self.height):
+            return tempw
+        else:
+            for i in range(self.width):
+                if(kouti.width + i < self.width):
+                    tempw += 1
+            for i in range(self.height):
+                if(kouti.height + i < self.height):
+                    temph += 1      
+            return tempw * temph
+
+class Square(Rectangle):
+    def __init__(self,plevra) -> None:
+        self.side = plevra
+        Rectangle.__init__(self,w=plevra,h=plevra)
+        
+    def __str__(self) -> str:
+        protasi = f'Square(side={self.side})'
+        return protasi
+    
+    def set_side(self,num) -> None:
+        self.side = num
+        Rectangle.set_height(num)
+        Rectangle.set_width(num)
+
+test = Rectangle(3,4)
+print(test.get_diagonal())
+print(test.get_picture())
+print(test)
+test2 = Square(3)
+print(test2.get_area())
+print(test2.get_picture())
+print(test2)
